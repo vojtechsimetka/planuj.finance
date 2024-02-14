2bd82e50b14efce26da37f58192b78a34cbb54b3
hlavkat <137988631+hlavkat@users.noreply.github.com>
2024-02-14 15:34:11 +0100

    feat: add age input and apy input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let age = $state(18)
+	interface Portfolio {
+		apy: number
+		feeSuccess: number
+		feeMangement: number
+	}
+	let portfolio = $state<Portfolio>({ apy: 0, feeSuccess: 0, feeMangement: 0 })
+</script>
+
+<div>
+	<label for="age">
+		<input type="text" name="age" bind:value={age} />
+	</label>
+	{age}
+	<label for="apy">
+		<input type="text" name="apy" bind:value={portfolio.apy} />
+	</label>
+</div>
