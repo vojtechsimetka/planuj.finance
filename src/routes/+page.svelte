4af2d931cad7a6785745369e802db530a73d223f
Vojtech Simetka <vojtech@simetka.cz>
2024-02-15 10:05:46 +0100

    fix: clicking on label now selects the input correctly

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,34 +7,33 @@
 		feeMangement: number
 	}
 	let portfolio = $state<Portfolio>({ apy: 0, feeSuccess: 0, feeMangement: 0 })
-    let deposits = $state([{}])
 </script>
 
 <div>
-	<label for="age">
+	<label>
 		Věk
-		<input type="text" name="age" bind:value={age} />
+		<input type="text" bind:value={age} />
 	</label>
-	<label for="apy">
+	<label>
 		Věk při ukončení investic
-		<input type="text" name="apy" bind:value={endAge} />
+		<input type="text" bind:value={endAge} />
 	</label>
-	<label for="apy">
+	<label>
 		Zhodnocení
-		<input type="text" name="apy" bind:value={portfolio.apy} />
+		<input type="text" bind:value={portfolio.apy} />
 	</label>
-	<label for="feeSuccess">
+	<label>
 		Poplatek za úspěch
-		<input type="text" name="feeSuccess" bind:value={portfolio.feeSuccess} />
+		<input type="text" bind:value={portfolio.feeSuccess} />
 	</label>
-	<label for="feeMangement">
+	<label>
 		Poplatek za správu
-		<input type="text" name="feeMangement" bind:value={portfolio.feeMangement} />
+		<input type="text" bind:value={portfolio.feeMangement} />
 	</label>
 </div>
 <div>
-    <h3>Vklady</h3>
-    <button>+</button>
-    <h3>Výběry</h3>
-    <button>+</button>
+	<h3>Vklady</h3>
+	<button>+</button>
+	<h3>Výběry</h3>
+	<button>+</button>
 </div>
