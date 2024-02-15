28ad7f92b78d987d53a152d2731418426d4f2308
Vojtech Simetka <vojtech@simetka.cz>
2024-02-15 10:37:25 +0100

    feat: deposit component

---
+++ src/lib/components/deposit.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type { Deposit } from '$lib/types'
+
+	interface Props {
+		deposit: Deposit
+		deleteDeposit: () => void
+	}
+	let { deposit, deleteDeposit } = $props<Props>()
+</script>
+
+<div>
+	<label>
+		Název
+		<input type="text" bind:value={deposit.name} />
+	</label>
+	<label>
+		Výše
+		<input type="text" bind:value={deposit.amount} />
+	</label>
+	<label>
+		Začátek
+		<input type="date" bind:value={deposit.startDate} />
+	</label>
+	<label>
+		Konec
+		<input type="date" bind:value={deposit.endDate} />
+	</label>
+	<label>
+		Frekvence
+		<input type="text" bind:value={deposit.frequency} />
+	</label>
+	<button onclick={deleteDeposit}>delete</button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
+	import DepositComponent from '$lib/components/deposit.svelte'
+	import type { Deposit } from '$lib/types'
+
 	let age = $state(18)
 	let endAge = $state(80)
+
 	interface Portfolio {
 		apy: number
 		feeSuccess: number
 		feeMangement: number
 	}
 	let portfolio = $state<Portfolio>({ apy: 0, feeSuccess: 0, feeMangement: 0 })
+
+	let deposits = $state<Deposit[]>([])
 </script>
 
 <div>
@@ -33,7 +39,19 @@
 </div>
 <div>
 	<h3>Vklady</h3>
-	<button>+</button>
+	<button
+		onclick={() => {
+			deposits.push({
+				name: `Deposit #${deposits.length}`,
+				amount: Math.floor(Math.random() * 50) * 1000,
+				startDate: new Date(),
+				frequency: 1,
+			})
+		}}>+</button
+	>
+	{#each deposits as deposit, i}
+		<DepositComponent {deposit} deleteDeposit={() => deposits.splice(i, 1)} />
+	{/each}
 	<h3>Výběry</h3>
 	<button>+</button>
 </div>
