f5911bee1f6d0702cf69458115156a0b3239039c
hlavkat <137988631+hlavkat@users.noreply.github.com>
2024-02-15 10:08:33 +0100

    feat: add more fee inputs (#4)
    * feat: add end of investment  and fees inputs
    
    * feat: add div for deposits and withdraws
    
    * fix: clicking on label now selects the input correctly
    
    ---------
    
    Co-authored-by: Vojtech Simetka <vojtech@simetka.cz>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	let age = $state(18)
+	let endAge = $state(80)
 	interface Portfolio {
 		apy: number
 		feeSuccess: number
@@ -9,11 +10,30 @@
 </script>
 
 <div>
-	<label for="age">
-		<input type="text" name="age" bind:value={age} />
+	<label>
+		Věk
+		<input type="text" bind:value={age} />
 	</label>
-	{age}
-	<label for="apy">
-		<input type="text" name="apy" bind:value={portfolio.apy} />
+	<label>
+		Věk při ukončení investic
+		<input type="text" bind:value={endAge} />
 	</label>
+	<label>
+		Zhodnocení
+		<input type="text" bind:value={portfolio.apy} />
+	</label>
+	<label>
+		Poplatek za úspěch
+		<input type="text" bind:value={portfolio.feeSuccess} />
+	</label>
+	<label>
+		Poplatek za správu
+		<input type="text" bind:value={portfolio.feeMangement} />
+	</label>
+</div>
+<div>
+	<h3>Vklady</h3>
+	<button>+</button>
+	<h3>Výběry</h3>
+	<button>+</button>
 </div>
