46eaba65ceff872a9ff43a8bb96d8bc49d0a8a23
Vojtech Simetka <vojtech@simetka.cz>
2024-02-16 12:28:15 +0100

    fix: don't allow editing non-existent withdrawals

--- src/lib/stores/details.svelte.ts
+++ src/lib/stores/details.svelte.ts
@@ -3,14 +3,38 @@ import type { Deposit, Portfolio, Withdrawal } from '$lib/types'
 interface DetailsStore {
 	age: number
 	endAge: number
+	inflation: number
 	portfolio: Portfolio
 	deposits: Deposit[]
 	withdrawals: Withdrawal[]
+
+	addDeposit: (deposit: Deposit) => void
+	removeDeposit: (index: number) => void
+	saveDeposit: (index: number, deposit: Deposit) => void
+	addWithdrawal: (withdrawal: Withdrawal) => void
+	removeWithdrawal: (index: number) => void
+	saveWithdrawal: (index: number, withdrawal: Withdrawal) => void
+}
+
+function add<T>(array: T[], item: T) {
+	array.push(item)
+}
+
+function remove<T>(array: T[], index: number) {
+	if (index >= 0 && index < array.length) {
+		array.splice(index, 1)
+	}
+}
+function save<T>(array: T[], index: number, item: T) {
+	if (index >= 0 && index < array.length) {
+		array[index] = item
+	}
 }
 
 export function withDetailsStore(): DetailsStore {
 	let age = $state(18)
 	let endAge = $state(80)
+	let inflation = $state(0)
 	let portfolio = $state<Portfolio>({ apy: 0, feeSuccess: 0, feeMangement: 0 })
 	let deposits = $state<Deposit[]>([])
 	let withdrawals = $state<Withdrawal[]>([])
@@ -27,6 +51,12 @@ export function withDetailsStore(): DetailsStore {
 		},
 		set endAge(value) {
 			endAge = value
+		},
+		get inflation() {
+			return inflation
+		},
+		set inflation(value) {
+			inflation = value
 		},
 		get portfolio() {
 			return portfolio
@@ -46,6 +76,25 @@ export function withDetailsStore(): DetailsStore {
 		},
 		set withdrawals(value) {
 			withdrawals = value
+		},
+
+		addDeposit(deposit) {
+			add(deposits, deposit)
+		},
+		removeDeposit(index) {
+			remove(deposits, index)
+		},
+		saveDeposit(index, deposit) {
+			save(deposits, index, deposit)
+		},
+		addWithdrawal(withdrawal) {
+			add(withdrawals, withdrawal)
+		},
+		removeWithdrawal(index) {
+			remove(withdrawals, index)
+		},
+		saveWithdrawal(index, withdrawal) {
+			save(withdrawals, index, withdrawal)
 		},
 	}
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,51 @@
 <script lang="ts">
+	import routes from '$lib/routes'
 	import { detailStore } from '$lib/stores/details.svelte'
 </script>
 
 <div>
 	<label>
 		Věk
-		<input type="text" bind:value={detailStore.age} />
+		<input type="number" bind:value={detailStore.age} />
 	</label>
 	<label>
 		Věk při ukončení investic
-		<input type="text" bind:value={detailStore.endAge} />
+		<input type="number" bind:value={detailStore.endAge} />
+	</label>
+	<label>
+		inflace
+		<input type="number" bind:value={detailStore.inflation} />
 	</label>
 	<label>
 		Zhodnocení
-		<input type="text" bind:value={detailStore.portfolio.apy} />
+		<input type="number" bind:value={detailStore.portfolio.apy} />
 	</label>
 	<label>
 		Poplatek za úspěch
-		<input type="text" bind:value={detailStore.portfolio.feeSuccess} />
+		<input type="number" bind:value={detailStore.portfolio.feeSuccess} />
 	</label>
 	<label>
 		Poplatek za správu
-		<input type="text" bind:value={detailStore.portfolio.feeMangement} />
+		<input type="number" bind:value={detailStore.portfolio.feeMangement} />
 	</label>
 </div>
 <div>
 	<h3>Vklady</h3>
-	<a href="/deposit">+</a>
+	<a href={routes.DEPOSIT()}>+</a>
 	{#each detailStore.deposits as deposit, i}
-		<div>{JSON.stringify(deposit)}<a href="/deposit/{i}">edit</a></div>
+		<div>
+			{JSON.stringify(deposit)}<a href={routes.DEPOSIT(i)}>edit</a><button
+				onclick={() => detailStore.removeDeposit(i)}>delete</button
+			>
+		</div>
 	{/each}
 	<h3>Výběry</h3>
-	<a href="/withdrawal">+</a>
+	<a href={routes.WITHDRAWAL()}>+</a>
 	{#each detailStore.withdrawals as withdrawal, i}
-		<div>{JSON.stringify(withdrawal)}<a href="/withdrawal/{i}">edit</a></div>
+		<div>
+			{JSON.stringify(withdrawal)}<a href={routes.WITHDRAWAL(i)}>edit</a><button
+				onclick={() => detailStore.removeWithdrawal(i)}>delete</button
+			>
+		</div>
 	{/each}
 </div>
