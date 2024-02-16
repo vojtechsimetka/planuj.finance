0908baeab2c06759d309f3202113553d3c9f277c
Vojtech Simetka <vojtech@simetka.cz>
2024-02-16 14:35:02 +0100

    test: add few tests for the effective interest rate function (#17)

--- src/lib/stores/results.svelte.ts
+++ src/lib/stores/results.svelte.ts
@@ -1,18 +1,5 @@
+import { getEffectiveInterestRate } from '$lib/calc'
 import { detailStore } from './details.svelte'
-
-export function getEffectiveInterestRate(
-	interestRate: number,
-	successFee: number,
-	managementFee: number,
-	inflation: number,
-) {
-	return (
-		(1 + (interestRate / 100) * (1 - successFee / 100)) *
-			(1 - managementFee / 100) *
-			(1 - inflation / 100) -
-		1
-	)
-}
 
 export function withResultsStore() {
 	const effectiveApy = $derived(
