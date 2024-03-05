a1a049969cd4d89566c7f824b13c74b19439e14c
Vojtech Simetka <vojtech@simetka.cz>
2024-03-05 15:15:37 +0100

    chore: disabled linter for $locale

--- src/lib/components/language.svelte
+++ src/lib/components/language.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
+	// FIXME: this should be removed once issue with using $ in svelte 5 is clearer (here the issue is $locale)
+	/* eslint svelte/valid-compile: 0 */
 	import { browser } from '$app/environment'
 	import { locale, locales } from 'svelte-i18n'
-	import { _ } from 'svelte-i18n'
 
 	// Assuming $locale is automatically set by svelte-i18n to the browser's setting, e.g., 'en-US'
-	$: if ($locale && !$locales.includes($locale)) {
-		$locale = $locale.split('-')[0] // Normalize 'en-US' to 'en'
-		if (!$locales.includes($locale)) {
-			$locale = $locales[0] // Default to the first locale if the normalized locale is still not in the list
+	$effect(() => {
+		if ($locale && !$locales.includes($locale)) {
+			$locale = $locale.split('-')[0] // Normalize 'en-US' to 'en'
+			if (!$locales.includes($locale)) {
+				$locale = $locales[0] // Default to the first locale if the normalized locale is still not in the list
+			}
 		}
-	}
-	$: if (browser && $locale) localStorage.setItem('user-lang', $locale)
+	})
+	$effect(() => {
+		if (browser && $locale) localStorage.setItem('user-lang', $locale)
+	})
 </script>
 
 <select bind:value={$locale}>
