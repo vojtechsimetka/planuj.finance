88e7b0c55b4375b72e5cd4d56b414070605ca523
hlavkat <137988631+hlavkat@users.noreply.github.com>
2024-03-11 18:33:54 +0100

    feat: styling withdrawal component (#61)
    * feat: styling withdrawal component
    
    * fix: use correct text for recurring withdrawal
    
    ---------
    
    Co-authored-by: Vojtech Simetka <vojtech@simetka.cz>

--- src/lib/components/withdrawal.svelte
+++ src/lib/components/withdrawal.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import type { WithdrawalForm } from '$lib/types'
 	import { _ } from 'svelte-i18n'
+	import Toggle from './toggle.svelte'
+	import Input from '$lib/components/input.svelte'
+	import Select from './select.svelte'
+	import Option from './option.svelte'
 
 	interface Props {
 		withdrawal: WithdrawalForm
@@ -8,37 +12,51 @@
 	let { withdrawal } = $props<Props>()
 </script>
 
-<div>
-	<label>
-		{$_('nameOfOperation')}
-		<input type="text" bind:value={withdrawal.name} />
-	</label>
-	<label>
-		{$_('amount')}
-		<input type="number" bind:value={withdrawal.amount} />
-	</label>
-	<label>
-		{$_('startDate')}
-		<input type="date" bind:value={withdrawal.startDate} />
-	</label>
-	<label>
-		{$_('isWithdrawalRecurring')}
-		<input type="checkbox" bind:checked={withdrawal.isRecurring} />
-	</label>
+<section>
+	<div class="grid">
+		<Input type="text" bind:value={withdrawal.name} placeholder={$_('nameOfOperation')}></Input>
+		<Input type="number" bind:value={withdrawal.amount} placeholder={$_('amount')}></Input>
+		<Input type="date" bind:value={withdrawal.startDate} placeholder={$_('startDate')}></Input>
+	</div>
+	<Toggle bind:checked={withdrawal.isRecurring}>{$_('isWithdrawalRecurring')}</Toggle>
 	{#if withdrawal.isRecurring}
-		<label>
-			{$_('endDate')}
-			<input type="date" bind:value={withdrawal.endDate} />
-		</label>
-		<label>
-			{$_('frequency')}
-			<select bind:value={withdrawal.frequency}>
-				<option value="day">{$_('daily')}</option>
-				<option value="week">{$_('weekly')}</option>
-				<option value="month">{$_('monthly')}</option>
-				<option value="year">{$_('yearly')}</option>
-			</select>
-		</label>
+		<div class="grid">
+			<Input type="date" bind:value={withdrawal.endDate} placeholder={$_('endDate')}></Input>
+			<Select bind:value={withdrawal.frequency} placeholder={$_('frequency')}>
+				<Option value="day">{$_('daily')}</Option>
+				<Option value="week">{$_('weekly')}</Option>
+				<Option value="month">{$_('monthly')}</Option>
+				<Option value="year">{$_('yearly')}</Option>
+			</Select>
+		</div>
 	{/if}
 	<slot />
-</div>
+</section>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+	html {
+		max-width: 1200px;
+		padding: 0 auto;
+	}
+	body {
+		padding: 1rem;
+	}
+	section {
+		padding-bottom: 1rem;
+		max-width: 1200px;
+		margin: 0 auto;
+	}
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(16.5rem, 1fr));
+		gap: 1rem;
+		justify-content: center;
+		width: 100%;
+	}
+</style>
--- src/routes/withdrawal/[[id]]/+page.svelte
+++ src/routes/withdrawal/[[id]]/+page.svelte
@@ -10,6 +10,8 @@
 	import { depositWithdrawalFormSchema, depositWithdrawalSchema } from '$lib/schemas'
 	import type { ZodError } from 'zod'
 	import { _ } from 'svelte-i18n'
+	import Button from '$lib/components/button.svelte'
+	import { Checkmark, TrashCan, Undo } from 'carbon-icons-svelte'
 
 	const index = get(page).params.id
 	const withdrawalIndex = isInt(index) ? Number.parseInt(index) : undefined
@@ -60,19 +62,74 @@
 </script>
 
 {#if (withdrawalIndex === undefined && Boolean(index)) || (withdrawalIndex !== undefined && detailStore.withdrawals.length <= withdrawalIndex)}
-	<h1>Výběr s indexem {index} jsme nenalezli</h1>
-	<a href="/">{$_('back')}</a>
+	<section>
+		<h5>Výběr s indexem {index} jsme nenalezli</h5>
+		<Button variant="secondary" onclick={cancel}><Undo size={24} /></Button>
+	</section>
 {:else if withdrawalIndex !== undefined}
-	<h1>{$_('editedWithdrawal')} {withdrawalIndex}</h1>
-	<WithdrawalComponent bind:withdrawal />
-	<button onclick={save} disabled={!formValid}>{$_('save')}</button>
-	<button onclick={deleteWithdrawal}>{$_('delete')}</button>
-	<button onclick={cancel}>{$_('cancel')}</button>
+	<section>
+		<h5>{$_('editedWithdrawal')} {withdrawalIndex}</h5>
+		<WithdrawalComponent bind:withdrawal />
+		<div class="buttons">
+			<Button variant="primary" onclick={save} disabled={!formValid}
+				><Checkmark size={24} />{$_('save')}</Button
+			>
+			<Button variant="secondary" onclick={deleteWithdrawal} disabled={!formValid}
+				><TrashCan size={24} />{$_('delete')}</Button
+			>
+			<Button variant="secondary" onclick={cancel}><Undo size={24} />{$_('cancel')}</Button>
+		</div>
+	</section>
 {:else}
-	<h1>{$_('newWithdrawal')}</h1>
-	<WithdrawalComponent bind:withdrawal />
-	<button onclick={add} disabled={!formValid}>{$_('add')}</button>
+	<section>
+		<h5>{$_('newWithdrawal')}</h5>
+		<WithdrawalComponent bind:withdrawal />
+		<div class="buttons">
+			<Button variant="primary" onclick={add} disabled={!formValid}>
+				<Checkmark size={24} />{$_('add')}
+			</Button>
+			<Button variant="secondary" onclick={cancel}><Undo size={24} />{$_('cancel')}</Button>
+		</div>
+	</section>
 {/if}
 {#if formErrors}
 	<pre>{JSON.stringify(formErrors, null, 2)}</pre>
 {/if}
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+	html {
+		max-width: 1200px;
+	}
+	body {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 1rem;
+	}
+	section {
+		padding-bottom: 1rem;
+		max-width: 1200px;
+		margin: 0 auto;
+	}
+	section > h5 {
+		color: var(--colors-ultraHigh, #303030);
+		/* h5 */
+		font-family: Arial;
+		font-size: 1rem;
+		font-style: normal;
+		font-weight: 700;
+		line-height: 1.5rem;
+		letter-spacing: 0.02rem;
+		padding-bottom: 1rem;
+	}
+	.buttons {
+		display: flex;
+		gap: 1rem;
+	}
+</style>
