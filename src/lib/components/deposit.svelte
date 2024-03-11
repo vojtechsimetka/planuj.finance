39c653ef3b9c5444d06e560e44c821006a511ac9
hlavkat <137988631+hlavkat@users.noreply.github.com>
2024-03-11 13:00:03 +0100

    feat: styling deposit component (#60)
    * feat: styling deposit component
    
    * fix: remove disabled rrom cnacel buttons, make delete deposit asynchronous
    
    ---------
    
    Co-authored-by: Vojtech Simetka <vojtech@simetka.cz>

--- src/lib/components/deposit.svelte
+++ src/lib/components/deposit.svelte
@@ -2,6 +2,9 @@
 	import type { DepositForm } from '$lib/types'
 	import { _ } from 'svelte-i18n'
 	import Toggle from './toggle.svelte'
+	import Input from '$lib/components/input.svelte'
+	import Select from './select.svelte'
+	import Option from './option.svelte'
 
 	interface Props {
 		deposit: DepositForm
@@ -9,34 +12,51 @@
 	let { deposit } = $props<Props>()
 </script>
 
-<div>
-	<label>
-		{$_('nameOfOperation')}
-		<input type="text" bind:value={deposit.name} />
-	</label>
-	<label>
-		{$_('amount')}
-		<input type="number" bind:value={deposit.amount} />
-	</label>
-	<label>
-		{$_('startDate')}
-		<input type="date" bind:value={deposit.startDate} />
-	</label>
+<section>
+	<div class="grid">
+		<Input type="text" bind:value={deposit.name} placeholder={$_('nameOfOperation')}></Input>
+		<Input type="number" bind:value={deposit.amount} placeholder={$_('amount')}></Input>
+		<Input type="date" bind:value={deposit.startDate} placeholder={$_('startDate')}></Input>
+	</div>
 	<Toggle bind:checked={deposit.isRecurring}>{$_('isDepositRecurring')}</Toggle>
 	{#if deposit.isRecurring}
-		<label>
-			{$_('endDate')}
-			<input type="date" bind:value={deposit.endDate} />
-		</label>
-		<label>
-			{$_('frequency')}
-			<select bind:value={deposit.frequency}>
-				<option value="day">{$_('daily')}</option>
-				<option value="week">{$_('weekly')}</option>
-				<option value="month">{$_('monthly')}</option>
-				<option value="year">{$_('yearly')}</option>
-			</select>
-		</label>
+		<div class="grid">
+			<Input type="date" bind:value={deposit.endDate} placeholder={$_('endDate')}></Input>
+			<Select bind:value={deposit.frequency} placeholder={$_('frequency')}>
+				<Option value="day">{$_('daily')}</Option>
+				<Option value="week">{$_('weekly')}</Option>
+				<Option value="month">{$_('monthly')}</Option>
+				<Option value="year">{$_('yearly')}</Option>
+			</Select>
+		</div>
 	{/if}
 	<slot />
-</div>
+</section>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+	html {
+		max-width: 1200px;
+		padding: 0 auto;
+	}
+	body {
+		padding: 1rem;
+	}
+	section {
+		padding-bottom: 1rem;
+		max-width: 1200px;
+		margin: 0 auto;
+	}
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(16.5rem, 1fr));
+		gap: 1rem;
+		justify-content: center;
+		width: 100%;
+	}
+</style>
--- src/routes/deposit/[[id]]/+page.svelte
+++ src/routes/deposit/[[id]]/+page.svelte
@@ -10,6 +10,8 @@
 	import type { DepositForm } from '$lib/types'
 	import { depositWithdrawalFormSchema, depositWithdrawalSchema } from '$lib/schemas'
 	import { _ } from 'svelte-i18n'
+	import Button from '$lib/components/button.svelte'
+	import { Checkmark, TrashCan, Undo } from 'carbon-icons-svelte'
 
 	const index = get(page).params.id
 	const depositIndex = isInt(index) ? Number.parseInt(index) : undefined
@@ -40,7 +42,7 @@
 	}
 	function deleteDeposit() {
 		if (depositIndex !== undefined) {
-			detailStore.removeDeposit(depositIndex)
+			setTimeout(() => detailStore.removeDeposit(depositIndex), 0)
 			goto(routes.HOME)
 		}
 	}
@@ -59,19 +61,75 @@
 </script>
 
 {#if (depositIndex === undefined && Boolean(index)) || (depositIndex !== undefined && detailStore.deposits.length <= depositIndex)}
-	<h1>Výběr s indexem {index} jsme nenalezli</h1>
-	<a href="/">{$_('back')}</a>
+	<section>
+		<h5>Vklad s indexem {index} jsme nenalezli</h5>
+		<Button variant="ghost" onclick={cancel} disabled={!formValid}><Undo size={24} /></Button>
+	</section>
 {:else if depositIndex !== undefined}
-	<h1>{$_('editedDeposit')} {depositIndex}</h1>
-	<DepositComponent bind:deposit />
-	<button onclick={save} disabled={!formValid}>{$_('save')}</button>
-	<button onclick={deleteDeposit}>{$_('delete')}</button>
-	<button onclick={cancel}>{$_('cancel')}</button>
+	<section>
+		<h5>{$_('editedDeposit')} {depositIndex}</h5>
+		<DepositComponent bind:deposit />
+		<div class="buttons">
+			<Button variant="primary" onclick={save} disabled={!formValid}
+				><Checkmark size={24} />{$_('save')}</Button
+			>
+			<Button variant="secondary" onclick={deleteDeposit} disabled={!formValid}
+				><TrashCan size={24} />{$_('delete')}</Button
+			>
+			<Button variant="secondary" onclick={cancel}><Undo size={24} />{$_('cancel')}</Button>
+		</div>
+	</section>
 {:else}
-	<h1>{$_('newDeposit')}</h1>
-	<DepositComponent bind:deposit />
-	<button onclick={add} disabled={!formValid}>{$_('add')}</button>
+	<section>
+		<h5>{$_('newDeposit')}</h5>
+		<DepositComponent bind:deposit />
+		<div class="buttons">
+			<Button variant="primary" onclick={add} disabled={!formValid}
+				><Checkmark size={24} />{$_('add')}</Button
+			>
+			<Button variant="secondary" onclick={cancel}><Undo size={24} />{$_('cancel')}</Button>
+		</div>
+	</section>
 {/if}
+
 {#if formErrors}
 	<pre>{JSON.stringify(formErrors, null, 2)}</pre>
 {/if}
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+	html {
+		max-width: 1200px;
+	}
+	body {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 1rem;
+	}
+	section {
+		padding-bottom: 1rem;
+		max-width: 1200px;
+		margin: 0 auto;
+	}
+	section > h5 {
+		color: var(--colors-ultraHigh, #303030);
+		/* h5 */
+		font-family: Arial;
+		font-size: 1rem;
+		font-style: normal;
+		font-weight: 700;
+		line-height: 1.5rem;
+		letter-spacing: 0.02rem;
+		padding-bottom: 1rem;
+	}
+	.buttons {
+		display: flex;
+		gap: 1rem;
+	}
+</style>
