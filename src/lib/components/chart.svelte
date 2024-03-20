d61da4f5a8734775bc044d7d300d59991ae37d3e
hlavkat <137988631+hlavkat@users.noreply.github.com>
2024-03-20 09:33:44 +0100

    feat: create Chart comppnent and extract all login from main page

---
+++ src/lib/components/chart.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import Chart, { type ChartDataset } from 'chart.js/auto'
+
+	interface Props {
+		labels: number[]
+		series: ChartDataset[]
+	}
+	let { labels, series }: Props = $props()
+
+	let canvas: HTMLCanvasElement | null = $state(null)
+	let chart: Chart | null = $state(null)
+
+	$effect(() => {
+		if (canvas && !chart) {
+			chart = new Chart(canvas, {
+				type: 'line',
+				data: {
+					labels: labels,
+					datasets: series,
+				},
+
+				options: {
+					scales: {
+						y: {
+							min: 0,
+						},
+					},
+					responsive: false,
+				},
+			})
+			chart.resize()
+		}
+		if (chart) {
+			chart.data.labels = labels
+			chart.data.datasets = series
+			chart.update()
+		}
+	})
+	let prevChartWidth: number = $state(0)
+	let actChartWidth: number = $state(0)
+	$effect(() => {
+		const interval = setInterval(() => {
+			if (actChartWidth !== prevChartWidth && chart) {
+				prevChartWidth = actChartWidth
+				chart.resize()
+			}
+		}, 1000)
+		return () => {
+			clearInterval(interval)
+		}
+	})
+</script>
+
+<div class="chart">
+	<canvas bind:this={canvas} bind:clientWidth={actChartWidth} />
+</div>
