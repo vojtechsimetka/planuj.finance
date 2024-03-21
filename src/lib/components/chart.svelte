822ada5dab997f861331b6cda22dc48533feb8d1
hlavkat <137988631+hlavkat@users.noreply.github.com>
2024-03-21 10:58:40 +0100

    feat: create Chart component and extract all logic from the main page (#121)
    * feat: create Chart comppnent and extract all login from main page
    
    * fix: bind div.chart instead of canvas, labels can be number or string array

---
+++ src/lib/components/chart.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import Chart, { type ChartDataset } from 'chart.js/auto'
+
+	interface Props {
+		labels: (number | string)[]
+		series: ChartDataset[]
+	}
+	let { labels, series }: Props = $props()
+
+	let canvas: HTMLCanvasElement | null = $state(null)
+	let chart: Chart | null = $state(null)
+
+	$effect(() => {
+		if (canvas && !chart) {
+			chart = new Chart(canvas, {
+				type: 'line',
+				data: {
+					labels: labels,
+					datasets: series,
+				},
+
+				options: {
+					scales: {
+						y: {
+							min: 0,
+						},
+					},
+					responsive: false,
+				},
+			})
+			chart.resize()
+		}
+		if (chart) {
+			chart.data.labels = labels
+			chart.data.datasets = series
+			chart.update()
+		}
+	})
+	let prevChartWidth: number = $state(0)
+	let actChartWidth: number = $state(0)
+	$inspect
+	$effect(() => {
+		const interval = setInterval(() => {
+			if (actChartWidth !== prevChartWidth && chart) {
+				prevChartWidth = actChartWidth
+				chart.resize()
+			}
+		}, 1000)
+		return () => {
+			clearInterval(interval)
+		}
+	})
+</script>
+
+<div class="chart" bind:clientWidth={actChartWidth}>
+	<canvas bind:this={canvas} />
+</div>
